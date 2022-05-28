1a51ea6d696243b0aff8787de7aef7fd684425b9
Peter Durham <peter@Peters-MacBook-Pro.local>
2022-05-28 14:39:42 -0700

    updated site placeholder text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bitcoin Stats" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Project around providing bitcoin statistics
         </p>
       </main>
 
